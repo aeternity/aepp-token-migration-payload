b1059c34d3f112e11a5054ce55e397b64f6b9012
Sadi Qevani <inaveq@gmail.com>
2018-11-09 19:39:47 +0100

    fixing 0x issue on data payload

--- src/app.vue
+++ src/app.vue
@@ -8,88 +8,99 @@
             Check the payload
           </template>
           <div class="text-div-top">
-          <h1>Payload</h1>
-          <p>
-            Your migration data is stored in a payload, which
-            functions as a receipt. <br/> Enter yours below, to
-            confirm that the information contained in the <br/> payload is correct.
-          </p>
-          <app-url v-model="payload">
-            <ae-toolbar fill="neutral">
-              a
-            </ae-toolbar>
-          </app-url>
+            <h1>Payload</h1>
+            <p>
+              Your migration data is stored in a payload, which
+              functions as a receipt. <br/> Enter yours below, to
+              confirm that the information contained in the <br/> payload is correct.
+            </p>
+            <app-url v-model="payload"/>
           </div>
         </app-panel>
         <app-panel padding primary>
           <div class="text-div-bottom">
-          <h1>Payload Information</h1>
-          <p>
-            The payload above contains the following information.<br/>
-            Please check if the information below is correct.
-          </p>
-        </div>
-        <div class="status-table-div">
-          <ul class="app-status-table">
-            <li>
-              <h3>Amount to be migrated</h3>
-              <div class="app-status-payload">
-                <span v-if="+tokenAmount">
-                  <big>{{tokenAmount}}</big><small>AE</small>
+            <h1>Payload Information</h1>
+            <p>
+              The payload above contains the following information.<br/>
+              Please check if the information below is correct.
+            </p>
+          </div>
+          <div class="status-table-div">
+            <ul class="app-status-table">
+              <li>
+                <h3>Amount to be migrated</h3>
+                <div class="app-status-payload">
+                  <span v-if="+tokenAmount">
+                    <big>{{tokenAmount}}</big>
+                    <small>AE</small>
+                  </span>
+                  <span v-else-if="!+tokenAmount && payload">
+                    <div>
+                      <ae-icon name="x-square" style="color:red" size="28px"/>
+                    </div>
+                  </span>
+                  <span v-else>
+                    <p>no data</p>
+                  </span>
+                </div>
+              </li>
+              <li>
+                <h3>Token Call Function</h3>
+                <div class="app-status-payload">
+                  <span v-if="callFunction && payload">
+                    <div>
+                      <ae-icon name="check" style="color:green" size="28px"/>
+                    </div>
+                  </span>
+                  <span v-else-if="!callFunction && payload">
+                    <div>
+                      <ae-icon name="x-square" style="color:red" size="28px"/>
+                    </div>
+                  </span>
+                  <span v-else>
+                    <p>no data</p>
+                  </span>
+                </div>
+              </li>
+              <li>
+                <h3>AE Migration Contract</h3>
+                <div class="app-status-payload">
+                  <span v-if="burnContract && payload">
+                    <div>
+                      <ae-icon name="check" style="color:green" size="28px"/>
+                    </div>
+                  </span>
+                  <span v-else-if="!burnContract && payload">
+                    <div>
+                      <ae-icon name="x-square" style="color:red" size="28px"/>
+                    </div>
+                  </span>
+                  <span v-else>
+                    <p>no data</p>
+                  </span>
+                </div>
+              </li>
+              <li>
+                <h3>Your AE Mainnet Address</h3>
+                <span v-if="aeAddress && payload">
+                  <div>
+                    <ae-icon name="check" style="color:green" size="28px"/>
+                  </div>
                 </span>
-                <span v-else-if="!+tokenAmount && payload">
-                  <div><ae-icon name="x-square" style="color:red" size="28px"/></div>
+                <span v-else-if="!aeAddress && payload">
+                  <div>
+                    <ae-icon name="x-square" style="color:red" size="28px"/>
+                  </div>
                 </span>
                 <span v-else>
                   <p>no data</p>
                 </span>
-              </div>
-            </li>
-            <li>
-              <h3>Token Call Function</h3>
-              <div class="app-status-payload">
-                <span v-if="callFunction && payload">
-                  <div><ae-icon name="check" style="color:green" size="28px"/></div>
-                </span>
-                <span v-else-if="!callFunction && payload">
-                  <div><ae-icon name="x-square" style="color:red" size="28px"/></div>
-                </span>
-                <span v-else>
-                  <p>no data</p>
-                </span>
-              </div>
-            </li>
-            <li>
-              <h3>AE Migration Contract</h3>
-              <div class="app-status-payload">
-                <span v-if="burnContract && payload">
-                  <div><ae-icon name="check" style="color:green" size="28px"/></div>
-                </span>
-                <span v-else-if="!burnContract && payload">
-                  <div><ae-icon name="x-square" style="color:red" size="28px"/></div>
-                </span>
-                <span v-else>
-                  <p>no data</p>
-                </span>
-              </div>
-            </li>
-            <li>
-              <h3>Your AE Mainnet Address</h3>
-              <span v-if="aeAddress && payload">
-                <div><ae-icon name="check" style="color:green" size="28px"/></div>
-              </span>
-              <span v-else-if="!aeAddress && payload">
-                <div><ae-icon name="x-square" style="color:red" size="28px"/></div>
-              </span>
-              <span v-else>
-                <p>no data</p>
-              </span>
-            </li>
-            <li class="app-align" v-if="aeAddress && payload">
-              <ae-address :value="aeAddress" length="flat"/>
-            </li>
-          </ul>
-        </div>
+              </li>
+              <li class="app-align" v-if="aeAddress && payload">
+                <ae-address :value="aeAddress" length="flat"/>
+              </li>
+            </ul>
+          </div>
         </app-panel>
       </app-panel>
     </app-view>
@@ -98,7 +109,6 @@
 <script>
 import Web3 from 'web3'
 import AeAddress from '@aeternity/aepp-components/dist/ae-address'
-import AeToolbar from '@aeternity/aepp-components/dist/ae-toolbar'
 import AeIcon from '@aeternity/aepp-components/dist/ae-icon'
 import AppUrl from './components/app-url.vue'
 
@@ -107,23 +117,27 @@ const $web3 = new Web3()
 export default {
   name: 'index',
   components: {
-    AeToolbar,
+    AppUrl,
     AeAddress,
-    AeIcon,
-    AppUrl
+    AeIcon
   },
   data: function () {
     return { payload: '' }
   },
   computed: {
-    tokenAmount: function () {
+    computedPayload: function () {
       if (this.payload && typeof this.payload !== 'string') {
         return false
       }
-
+      if (this.payload.substring(0, 2) === '0x') {
+        return this.payload.substring(2)
+      }
+      return this.payload
+    },
+    tokenAmount: function () {
       try {
         return $web3.utils.fromWei(
-          $web3.utils.toBN('0x' + this.payload.substr(72, 64)
+          $web3.utils.toBN('0x' + this.computedPayload.substr(72, 64)
           ).toString()
         )
       } catch (e) {
@@ -131,25 +145,15 @@ export default {
       }
     },
     callFunction: function () {
-      if (this.payload && typeof this.payload !== 'string') {
-        return false
-      }
-      return this.payload.substr(0, 8) === process.env.VUE_APP_AE_CALL_FUNCTION
+      return this.computedPayload.substr(0, 8) === process.env.VUE_APP_AE_CALL_FUNCTION
     },
     burnContract: function () {
-      if (this.payload && typeof this.payload !== 'string') {
-        return false
-      }
-      return this.payload.substr(8, 64) === process.env.VUE_APP_AE_BURN_CONTRACT
+      return this.computedPayload.substr(8, 64) === process.env.VUE_APP_AE_BURN_CONTRACT
     },
     aeAddress: function () {
-      if (this.payload && typeof this.payload !== 'string') {
-        return false
-      }
-
       try {
         return $web3.utils.hexToString(
-          $web3.eth.abi.decodeParameter('bytes', this.payload.substr(136))
+          $web3.eth.abi.decodeParameter('bytes', this.computedPayload.substr(136))
         )
       } catch (e) {
         return false
